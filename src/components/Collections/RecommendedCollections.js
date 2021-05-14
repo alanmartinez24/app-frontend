8f15f1b1e4c5a7891e3dd5d8c5ab2fd1d7d7eab3
Eric Heydorn Gorski <er.h.gorski@gmail.com>
2021-05-14 14:51:23 -0400

    initial

---
+++ src/components/Collections/RecommendedCollections.js
@@ -0,0 +1,77 @@
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { Typography, Grid } from '@material-ui/core'
+import Img from 'react-image'
+import { withStyles } from '@material-ui/core/styles'
+
+const AWS_DEFAULT_COLLECTION_IMG_URLS = [...Array(5)].map((_, i) => `https://app-gradients.s3.amazonaws.com/gradient${i + 1}.png`)
+const getRandomGradientImg = () => `${AWS_DEFAULT_COLLECTION_IMG_URLS[Math.floor(Math.random() * AWS_DEFAULT_COLLECTION_IMG_URLS.length)]}`
+
+const styles = theme => ({
+  recommendedImg: {
+    height: '60px',
+    width: '60px',
+    objectFit: 'cover',
+    marginTop: '10px',
+    borderRadius: '5px',
+    [theme.breakpoints.down('md')]: {
+      height: '50px',
+      width: '50px'
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '40px',
+      width: '40px'
+    }
+  },
+  recommendedContainer: {
+    borderRadius: 10,
+    '&:hover': {
+      background: '#fafafa05'
+    }
+  }
+})
+
+const RecommendedCollections = ({ classes, collection }) => {
+  const fmtCollectionName = collection && collection.name && collection.name.replace(/\s+/g, '-').toLowerCase()
+
+  return (
+    <Link
+      to={`/collections/${fmtCollectionName}/${collection._id}`}
+      style={{ textDecoration: 'none', color: '#fff' }}
+    >
+      <Grid
+        container
+        direction='row'
+        justify='flex-start'
+        alignItems='center'
+        spacing={2}
+        className={classes.recommendedContainer}
+      >
+        <Grid item
+          xs={2}
+        >
+          <Img
+            src={[collection.imgSrcUrl, getRandomGradientImg()]}
+            alt='thumbnail'
+            className={classes.recommendedImg}
+          />
+        </Grid>
+        <Grid item
+          xs={10}
+        >
+          <Typography variant='h5'>{collection.name}</Typography>
+          <Typography variant='body2'>{collection.owner}</Typography>
+        </Grid>
+      </Grid>
+    </Link>
+  )
+}
+
+RecommendedCollections.propTypes = {
+  classes: PropTypes.object.isRequired,
+  collection: PropTypes.array.isRequired
+}
+
+export default withStyles(styles)(RecommendedCollections)
