5700bcc8906c575a1fda8179e2850ce82c68b4ab
Eric Gorski <ericgorski@Erics-MacBook-Pro.local>
2021-06-09 11:33:12 +0200

    refactor: collectionPostMeu to class based component

--- src/components/Collections/CollectionPostMenu.js
+++ src/components/Collections/CollectionPostMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { IconButton, MenuItem, Menu, Snackbar, SnackbarContent } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
@@ -6,8 +6,7 @@ import axios from 'axios'
 import wallet from '../../eos/scatter/scatter.wallet.js'
 import CollectionPostDialog from './CollectionPostDialog.js'
 import { withStyles } from '@material-ui/core/styles'
-import { useSelector } from 'react-redux'
-import { createSelector } from 'reselect'
+import { connect } from 'react-redux'
 
 const BACKEND_API = process.env.BACKEND_API
 
@@ -26,251 +25,164 @@ const styles = theme => ({
   }
 })
 
-const selectUserCollections = createSelector(
-    state => {
-    console.log(state, 'selectorState')
-    if (!state) return []
-    const { account: ethAccount } = state.ethAuth
-    const scatterIdentity = state.scatterRequest && state.scatterRequest.account
-    let _account = scatterIdentity || state.ethAccount
-
-    console.log(_account, '_account')
-    if (!scatterIdentity && ethAccount) {
-      _account = { name: ethAccount._id, authority: 'active' }
-    }
-    if (_account) {
-      return state.userCollections[_account.name]
-    }
-    return []
-    },
-    state => state
-    )
-  /* const userCollections = state.userCollections
-  const userCollData = userCollections[accountName]
-  if (userCollData) {
-    return userCollData.collections
+class CollectionPostMenu extends Component {
+  state = {
+    anchorEl: null,
+    dialogOpen: false,
+    snackbarMsg: ''
   }
-  return [] */
 
-const CollectionPostMenu = ({ postid, classes }) => {
-  if (!postid) return null
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [dialogOpen, setDialogOpen] = useState(false)
-  const [snackbarMsg, setSnackbarMsg] = useState('')
-  const ethAuth = useSelector(state => state.ethAuth)
-  const account = useSelector(state => {
-    const { account: ethAccount } = state.ethAuth
+  handleMenuClick = ({ currentTarget }) => this.setState({ anchorEl: currentTarget })
+  handleMenuClose = () => this.setState({ anchorEl: null })
+  handleDialogOpen = () => this.setState({ dialogOpen: true })
+  handleDialogClose = () => this.setState({ dialogOpen: false })
+  handleSnackbarOpen = (msg) => this.setState({ snackbarMsg: msg })
+  handleSnackbarClose = () => this.setState({ snackbarMsg: '' })
 
-    const scatterIdentity = state.scatterRequest && state.scatterRequest.account
-    let _account = scatterIdentity || state.ethAccount
-
-    if (!scatterIdentity && ethAccount) {
-      _account = { name: ethAccount._id, authority: 'active' }
-    }
-    return _account
-  }, null)
-
-  if (!account) { return }
-  const userCollections = useSelector(state => selectUserCollections(state))
-
-  console.log(userCollections, 'userCollections')
-  const collectionsPageId = window.location.href.split('/').pop()
-/*
-  const selectAddedCollections = createSelector(state =>
-  state.userCollections[account.name]
-  , collections => {
-    if (collections && collections.length !== 0) {
-     collections.filter(collection => collection.postIds.includes(postid))
-    }
-    return collections
-  }
-)
-  const addedCollections = useSelector(state => selectAddedCollections(state))
-  console.log(addedCollections, 'COLL DATA') */
-
-  /*
-    collections.map((collection) => {
-            if (!collection.postIds.includes(postid) && collectionsPageId !== collection._id) {
-            return (
-              <MenuItem dense
-                key={collection._id}
-                className={classes.menuItem}
-                onClick={() => addToCollection(collection)}
-              >
-                Add to {collection.name}
-              </MenuItem>
-  */
-  const menuOpen = Boolean(anchorEl)
-
-  const handleMenuClick = ({ currentTarget }) => setAnchorEl(currentTarget)
-  const handleMenuClose = () => setAnchorEl(null)
-  const handleDialogOpen = () => setDialogOpen(true)
-  const handleDialogClose = () => setDialogOpen(false)
-
-  const handleSnackbarOpen = (msg) => setSnackbarMsg(msg)
-  const handleSnackbarClose = () => setSnackbarMsg('')
-  const accountName = account && account.name
-
-  // function useCompare (val) {
-  //   const prevVal = usePrevious(val)
-  //   return prevVal !== val
-  // }
-
-  // function usePrevious (value) {
-  //   const ref = useRef()
-  //   useEffect(() => {
-  //     ref.current = value
-  //   })
-  //   return ref.current
-  // }
-  // const needsUpdate = useCompare(userCollections)
-  // console.log('userCollections :>> ', userCollections)
-  // console.log('needsUpdate :>> ', needsUpdate)
-
-  // useEffect(() => {
-  //   console.log('USE EFFECT IS TRIGGERD')
-  //   if (needsUpdate) {
-  //   // if (userCollections[accountName].length > 0) return
-  //   // const userCollectionData = (await axios.get(`${BACKEND_API}/accounts/${accountName}/collections`)).data
-  //   setMenuCollections(userCollections[accountName])
-  //   console.log('menuCollections :>> ', menuCollections)
-  //   }
-  // }, [needsUpdate])
-  const collections = { ...userCollections }
-
-  const fetchAuthToken = async () => {
-    if (ethAuth) return ethAuth
+  fetchAuthToken = async () => {
+    if (this.props.ethAuth) return this.props.ethAuth
     else {
       const { eosname, signature } = await wallet.scatter.getAuthToken()
       return { eosname, signature }
     }
   }
 
-  const addToCollection = async (collection) => {
+  addToCollection = async (collection) => {
     try {
-      handleMenuClose()
-      const authToken = await fetchAuthToken()
-      const params = { postId: postid, ...authToken }
+      this.handleMenuClose()
+      const authToken = await this.fetchAuthToken()
+      const params = { postId: this.props.postid, ...authToken }
       await axios.put(`${BACKEND_API}/collections/${collection._id}`, params)
-      handleSnackbarOpen(`Succesfully added to ${collection.name}`)
+      this.handleSnackbarOpen(`Succesfully added to ${collection.name}`)
     } catch (err) {
       console.error(err)
-      handleSnackbarOpen(`An error occured. Try again later.`)
+      this.handleSnackbarOpen(`An error occured. Try again later.`)
     }
   }
 
-  const removeFromCollection = async (collection) => {
+  removeFromCollection = async (collection) => {
     try {
-      handleMenuClose()
-      const authToken = await fetchAuthToken()
-      const params = { postId: postid, ...authToken }
+      this.handleMenuClose()
+      const authToken = await this.fetchAuthToken()
+      const params = { postId: this.props.postid, ...authToken }
       await axios.put(`${BACKEND_API}/collections/remove/${collection._id}`, params)
-      handleSnackbarOpen(`Succesfully removed post from ${collection.name}`)
+      this.handleSnackbarOpen(`Succesfully removed post from ${collection.name}`)
     } catch (err) {
       console.error(err)
-      handleSnackbarOpen(`An error occured. Try again later.`)
+      this.handleSnackbarOpen(`An error occured. Try again later.`)
     }
   }
 
-  return (
-    <>
-      <Snackbar
-        autoHideDuration={4000}
-        onClose={handleSnackbarClose}
-        open={!!snackbarMsg}
-      >
-        <SnackbarContent
-          className={classes.snack}
-          message={snackbarMsg}
-        />
-      </Snackbar>
-      <IconButton
-        aria-label='more'
-        aria-controls='long-menu'
-        aria-haspopup='true'
-        onClick={handleMenuClick}
-        className={classes.button}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Menu
-        id='long-menu'
-        anchorEl={anchorEl}
-        keepMounted
-        open={menuOpen}
-        onClose={handleMenuClose}
-        PaperProps={{
-          style: {
-            width: '35ch',
-            backgroundColor: 'black'
-          }
-        }}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right'
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right'
-        }}
-      >
-        <MenuItem dense
-          onClick={handleDialogOpen}
-          className={classes.menuItem}
+  render () {
+    const { postid, classes, ethAuth, account, collections } = this.props
+    if (!postid) return null
+    const { anchorEl, snackbarMsg, dialogOpen } = this.state
+    const accountName = account && account.name
+    const collectionsPageId = window.location.href.split('/').pop()
+    const menuOpen = Boolean(anchorEl)
+    return (
+      <>
+        <Snackbar
+          autoHideDuration={4000}
+          onClose={this.handleSnackbarClose}
+          open={!!snackbarMsg}
         >
-          New Collection...
-        </MenuItem>
-        {collections && accountName && collections.length > 0 && (
-          collections.map((collection) => {
-            if (!collection.postIds.includes(postid) && collectionsPageId !== collection._id) {
-            return (
-              <MenuItem dense
-                key={collection._id}
-                className={classes.menuItem}
-                onClick={() => addToCollection(collection)}
-              >
-                Add to {collection.name}
-              </MenuItem>
-          )
-            } else {
+          <SnackbarContent
+            className={classes.snack}
+            message={snackbarMsg}
+          />
+        </Snackbar>
+        <IconButton
+          aria-label='more'
+          aria-controls='long-menu'
+          aria-haspopup='true'
+          onClick={this.handleMenuClick}
+          className={classes.button}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id='long-menu'
+          anchorEl={anchorEl}
+          keepMounted
+          open={menuOpen}
+          onClose={this.handleMenuClose}
+          PaperProps={{
+            style: {
+              width: '35ch',
+              backgroundColor: 'black'
+            }
+          }}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right'
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}
+        >
+          <MenuItem dense
+            onClick={this.handleDialogOpen}
+            className={classes.menuItem}
+          >
+            New Collection...
+          </MenuItem>
+          {collections && accountName && collections.length > 0 && (
+            collections.map((collection) => {
+              if (!collection.postIds.includes(postid) && collectionsPageId !== collection._id) {
               return (
                 <MenuItem dense
                   key={collection._id}
                   className={classes.menuItem}
-                  onClick={() => removeFromCollection(collection)}
+                  onClick={() => this.addToCollection(collection)}
                 >
-                  Remove from {collection.name}
+                  Add to {collection.name}
                 </MenuItem>
-              )
-            }
-        })
-        )}
-      </Menu>
-      <CollectionPostDialog
-        account={account}
-        dialogOpen={dialogOpen}
-        postid={postid}
-        ethAuth={ethAuth}
-        handleDialogClose={handleDialogClose}
-      />
-    </>
+            )
+              } else {
+                return (
+                  <MenuItem dense
+                    key={collection._id}
+                    className={classes.menuItem}
+                    onClick={() => this.removeFromCollection(collection)}
+                  >
+                    Remove from {collection.name}
+                  </MenuItem>
+                )
+              }
+          })
+          )}
+        </Menu>
+        <CollectionPostDialog
+          account={account}
+          dialogOpen={dialogOpen}
+          postid={postid}
+          ethAuth={ethAuth}
+          handleDialogClose={this.handleDialogClose}
+        />
+      </>
   )
+      }
 }
 
 CollectionPostMenu.propTypes = {
   postid: PropTypes.string,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  ethAuth: PropTypes.object,
+  account: PropTypes.object.isRequired,
+  collections: PropTypes.array.isRequired
 }
 
-// const mapStateToProps = (state, ownProps) => {
-//   const ethAuth = state.ethAuth.account ? state.ethAuth : null
-//   console.log(state, 'THIS IS THE STATE')
-//   const userCollections = state.userCollections
+const mapStateToProps = (state, ownProps) => {
+  const ethAuth = state.ethAuth.account ? state.ethAuth : null
+  console.log(state, 'THIS IS THE STATE')
+  const userCollections = state.userCollections
 
-//   return {
-//     ethAuth,
-//     userCollections: userCollections || []
-//   }
-// }
+  return {
+    ethAuth,
+    userCollections: userCollections || []
+  }
+}
 
-export default withStyles(styles)(CollectionPostMenu)
+export default connect(mapStateToProps)(withStyles(styles)(CollectionPostMenu))
