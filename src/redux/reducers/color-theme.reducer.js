bea7e44162da62779c9119f5461ca6fbd384c5e8
Vernon Johnson <vtj2105@columbia.edu>
2021-07-13 10:29:33 -0500

    fix: color theme reducer patch

--- src/redux/reducers/color-theme.reducer.js
+++ src/redux/reducers/color-theme.reducer.js
@@ -1,4 +1,4 @@
-import { collectionsConstants as constants } from '../constants'
+import { colorThemeConstants as constants } from '../constants'
 import produce from 'immer'
 
 const initalState = { active: false }
@@ -7,7 +7,7 @@ export function lightMode (state = initalState, action) {
   return produce(state, draft => {
     switch (action.type) {
       case constants.TOGGLE_COLOR_THEME:
-        draft[state.active] = !state.active
+        draft.active = !state.active
         break
       default:
         return state
