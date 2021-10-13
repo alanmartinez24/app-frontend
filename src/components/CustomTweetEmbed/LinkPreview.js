ca3b90074a693c1a002524023269fa0f3960ca45
Eric Gorski <er.h.gorski@gmail.com>
2021-10-13 11:20:49 -0400

    fix: twit img overflow

--- src/components/CustomTweetEmbed/LinkPreview.js
+++ src/components/CustomTweetEmbed/LinkPreview.js
@@ -2,12 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const LinkPreview = ({ description, image, title, url, caption, classes, size }) => {
-  const getDomain = (str) => {
-      let a = document.createElement('a')
-      a.href = str
-      return a.hostname
-    }
-
   return (
     <div className={size === 'large' ? classes.LinkPreviewMainLarge : classes.LinkPreviewMain}>
       <a className={classes.LinkPreviewAnchor}
@@ -19,16 +13,6 @@ const LinkPreview = ({ description, image, title, url, caption, classes, size })
             src={image}
             alt={description}
           />
-        </div>
-      </a>
-      <a className={classes.LinkPreviewAnchor}
-        href={url}
-        target='_blank'
-      >
-        <div className={size === 'large' ? classes.LinkPreviewContentLarge : classes.LinkPreviewContent}>
-          <div className={classes.LinkPreviewTitle}>{title}</div>
-          <div className={classes.LinkPreviewText}>{`${description && description.substring(0, 50)}...` || `${caption && caption.substring(0, 50)}...`} </div>
-          <div className={classes.LinkPreviewURL}>{url && getDomain(url)}</div>
         </div>
       </a>
     </div>
