23a7f9acbdb4aaf01a3d3f5e739f0b2717ee1681
[nick_fasulo] <fasulo.nick@gmail.com>
2022-02-28 09:33:40 -0500

    added adornments to YupButton.js

--- src/components/Miscellaneous/YupButton.js
+++ src/components/Miscellaneous/YupButton.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from '@material-ui/core'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 
-const YupButton = ({ variant, children, ...restProps }) => {
+const YupButton = ({ variant, children, adornment, ...restProps }) => {
+  const rightAdornment = adornment === 'right icon' || adornment === 'both icons' ? <ArrowForwardIcon /> : null
+  const leftAdornment = adornment === 'left icon' || adornment === 'both icons' ? <ArrowForwardIcon /> : null
+
+  if (adornment === 'one icon') {
+    return <Button variant={variant || null}
+      {...restProps}
+           ><ArrowForwardIcon /></Button>
+  }
+
   return (
-    <Button variant={variant || null}
+    <Button
+      variant={variant || null}
       {...restProps}
     >
+      {leftAdornment}
       {children}
+      {rightAdornment}
     </Button>
   )
 }
